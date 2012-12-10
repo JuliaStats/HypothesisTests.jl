805fb8a585afc0ddd000a8c66961cb26876d7062
Simon Kornblith <simon@simonster.com>
2012-12-09 23:23:06 -0500

    Add last set of reversals

--- test/test.jl
+++ test/test.jl
@@ -52,6 +52,8 @@ using Wilcoxon, Test
 @test abs(p_value([2:2:16, -1, 1], [1:10], ApproximateSignedRankTest) - 0.4148) <= 1e-4
 
 # Approximate with ties
-@test abs(p_value([1:10], [2:11], ApproximateSignedRankTest) - 0.001904) <= 1e-6
 @test abs(p_value([1:10], [1:10], ApproximateSignedRankTest) - 1) <= 1e-4
+@test abs(p_value([1:10], [2:11], ApproximateSignedRankTest) - 0.001904) <= 1e-6
+@test abs(p_value([2:11], [1:10], ApproximateSignedRankTest) - 0.001904) <= 1e-6
 @test abs(p_value([1:10], [1:5, ones(5)], ApproximateSignedRankTest) - 0.05906) <= 1e-5
+@test abs(p_value([1:5, ones(5)], [1:10], ApproximateSignedRankTest) - 0.05906) <= 1e-5
