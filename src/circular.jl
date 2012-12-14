71e38fa94f67622e1d5305f42e5d71ff94ce3246
Simon Kornblith <simon@simonster.com>
2012-12-14 02:10:55 -0500

    Add toroidal-linear association test, along with insufficiently comprehensive test

--- src/circular.jl
+++ src/circular.jl
@@ -2,8 +2,10 @@
 # Rayleigh test of randomness against a unimodal alternative
 #
 # For reference see:
-# N.I. Fisher, Statistical Analysis of Circular Data. Cambridge: Cambridge 
-# University Press, 1995.
+# Fisher, N. I., & Lee, A. J. (1983). A correlation coefficient for
+#     circular data. Biometrika, 70(2), 327–332. doi:10.2307/2335547
+# Fisher, N. I., Statistical Analysis of Circular Data. Cambridge:
+#     Cambridge University Press, 1995.
 #
 # Copyright (C) 2012   Simon Kornblith
 #
@@ -26,9 +28,10 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-export RayleighTest
+export RayleighTest, TLinearAssociation
 
-# Rayleigh test of uniformity against a unimodal alternative
+## RAYLEIGH TEST OF UNIFORMITY AGAINST AN UNSPECIFIED UNIMODAL ALTERNATIVE
+
 type RayleighTest <: HypothesisTest
 	Rbar::Float64
 	Z::Float64
@@ -66,3 +69,107 @@ function RayleighTest{S <: Number}(samples::Vector{S})
 	p = p_value(RayleighTest, Z, length(samples))
 	RayleighTest(s/n, Z, p)
 end
+
+## TEST OF T-LINEAR CIRCULAR-CIRCULAR ASSOCIATION
+
+type TLinearAssociation <: HypothesisTest
+	rho_t::Float64
+	p_value::Float64
+end
+test_name(::Type{TLinearAssociation}) = "T-linear test of circular-circular association"
+
+check_same_length(x::Vector, y::Vector) = if length(x) != length(y)
+		error("Vectors must be the same length")
+	end
+
+# Calculate T from Fisher, 1993
+function tlinear_T{S <: Real, T <: Real}(theta::Vector{S}, phi::Vector{T})
+	A = sum(cos(theta).*cos(phi))
+	B = sum(sin(theta).*sin(phi))
+	C = sum(cos(theta).*sin(phi))
+	D = sum(sin(theta).*cos(phi))
+	A*B-C*D
+end
+
+# For large samples, compute the distribution and statistic of T
+function tlinear_large_sample_dist_and_stat{S <: Real, T <: Real}(theta::Vector{S}, phi::Vector{T})
+	rho_t = test_statistic(TLinearAssociation, theta, phi)
+	theta_resultant = sum(exp(im*theta))
+	phi_resultant = sum(exp(im*phi))
+	if abs(theta_resultant) <= eps() || abs(phi_resultant) <= eps()
+		# "If either distribution has a mean resultant length 0...the statistic has
+		# approximately a double exponential distribution with density 1/2*exp(-abs(x))
+		return (Laplace(), rho_t)
+	end
+
+	alpha_2_theta = mean(cos(2*(theta-angle(theta_resultant))))
+	beta_2_theta = mean(sin(2*(theta-angle(theta_resultant))))
+	alpha_2_phi = mean(cos(2*(phi-angle(phi_resultant))))
+	beta_2_phi = mean(sin(2*(phi-angle(phi_resultant))))
+	U_theta = (1-alpha_2_theta^2-beta_2_theta^2)
+	U_phi = (1-alpha_2_phi^2-beta_2_phi^2)
+	V_theta = abs(theta_resultant/n)^2*(1-alpha_2_theta)
+	V_phi = abs(phi_resultant/n)^2*(1-alpha_2_phi)
+	Z = sqrt(n)*U_theta*U_phi*rho_t/sqrt(V_theta*V_phi)
+	(Normal(), Z)
+end
+
+# Angles (in radians)
+function test_statistic{S <: Real, T <: Real}(::Type{TLinearAssociation}, theta::Vector{S}, phi::Vector{T})
+	check_same_length(theta, phi)
+	# Notation drawn from Fisher, 1993
+	n = length(theta)
+	E = sum(cos(2*theta))
+	F = sum(sin(2*theta))
+	G = sum(cos(2*phi))
+	H = sum(sin(2*phi))
+	4*tlinear_T(theta, phi)/sqrt((n^2 - E^2 - F^2)*(n^2-G^2-H^2))
+end
+
+# p-values
+for (fn, transform, comparison, distfn) in ((:p_value, :abs, :>, :ccdf),
+	                                         (:left_p_value, :+, :<, :cdf),
+	                                         (:right_p_value, :+, :>, :ccdf))
+	@eval begin
+		function $(fn){S <: Real, T <: Real}(::Type{TLinearAssociation}, theta::Vector{S}, phi::Vector{T})
+			check_same_length(theta, phi)
+			n = length(theta)
+			if n < 25
+				# "For n < 25, use a randomisation test based on the quantity T = AB - CD"
+				T = $(transform)(tlinear_T(theta, phi))
+				greater = 0
+
+				if n <= 8
+					# Exact permutation test
+					nperms = factorial(n)
+					for i = 1:maxn
+						greater += $(comparison)($(transform)(tlinear_T(nthperm(theta, i), phi)), T)
+					end
+					return greater / nperms
+				end
+
+				# Approximate permutation test
+				const nperms = 10000
+				thetap = copy(theta)
+				for i = 1:nperms
+					greater += $(comparison)($(transform)(tlinear_T(shuffle!(thetap), phi)), T)
+				end
+				return greater / nperms
+			end
+
+			(dist, stat) = tlinear_large_sample_dist_and_stat(theta, phi)
+			p = 2 * $(distfn)(dist, $(transform)(stat))
+		end
+	end
+end
+
+# Complex numbers
+for fn in (:test_statistic, :p_value, :left_p_value, :right_p_value)
+	@eval begin
+		$(fn){S <: Complex, T <: Complex}(::Type{TLinearAssociation}, x::Vector{S}, y::Vector{T}) =
+			$(fn)(angle(x), angle(y))
+	end
+end
+
+TLinearAssociation{S <: Number, T <: Number}(theta::Vector{S}, phi::Vector{T}) =
+	TLinearAssociation(test_statistic(TLinearAssociation, theta, phi), p_value(TLinearAssociation, theta, phi))
--- test/circular.jl
+++ test/circular.jl
@@ -2,11 +2,26 @@ load("test")
 load("src/HypothesisTests")
 using HypothesisTests, Test
 
-# Fisher example 4.11
+# Fisher, 1993 example 4.11
 @test abs(p_value(RayleighTest, 60*0.2370^2, 60) - 0.034) <= 0.001
-# Fisher example 4.12
-@test abs(p_value(RayleighTest, [2, 9, 18, 24, 30, 35, 35, 39, 39, 44,
- 44, 49, 56, 70, 76, 76, 81, 86, 91, 112,
- 121, 127, 133, 134, 138, 147, 152, 157, 166, 171,
- 177, 187, 206, 210, 211, 215, 238, 246, 269, 270,
- 285, 292, 305, 315, 325, 328, 329, 343, 354, 359]*pi/180) - 0.20) <= 0.01
+
+# Fisher, 1993 example 4.12
+@test abs(p_value(RayleighTest,
+    [2, 9, 18, 24, 30, 35, 35, 39, 39, 44,
+	44, 49, 56, 70, 76, 76, 81, 86, 91, 112,
+	121, 127, 133, 134, 138, 147, 152, 157, 166, 171,
+	177, 187, 206, 210, 211, 215, 238, 246, 269, 270,
+	285, 292, 305, 315, 325, 328, 329, 343, 354, 359]
+	*pi/180) - 0.20) <= 0.01
+
+# Fisher, 1993 example 6.8
+wind_direction_6am =
+	[356, 97, 211, 232, 343, 292, 157, 302, 335, 302,
+	324, 85, 324, 340, 157, 238, 254, 146, 232, 122,
+	329]*pi/180
+wind_direction_12pm =
+	[119, 162, 221, 259, 270, 29, 97, 292, 40, 313,
+	94, 45, 47, 108, 221, 270, 119, 248, 270, 45,
+	23]*pi/180
+@test abs(test_statistic(TLinearAssociation, wind_direction_6am, wind_direction_12pm) - 0.191) < 0.001
+@test abs(p_value(TLinearAssociation, wind_direction_6am, wind_direction_12pm) - 0.01) < 0.01
