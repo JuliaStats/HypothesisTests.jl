be6d10ec1837e780cd5d16e8f843e7fd951b4e73
Simon Kornblith <simon@simonster.com>
2015-09-09 17:41:04 -0400

    Precompile

--- src/HypothesisTests.jl
+++ src/HypothesisTests.jl
@@ -22,6 +22,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
+
 module HypothesisTests
 
 using Distributions, Roots, StatsBase, Compat
--- src/common.jl
+++ src/common.jl
@@ -22,7 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const libRmath = VERSION >= v"0.3.0-" ? "libRmath-julia" : "libRmath"
+const libRmath = "libRmath-julia"
 
 # PROBABILITY FUNCTIONS
 
