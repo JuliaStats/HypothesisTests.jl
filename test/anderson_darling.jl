173e038d1455c393dcd6d4d968d995114950be83
wildart <wildart@gmail.com>
2015-11-12 20:22:38 -0500

    added more tests

--- test/anderson_darling.jl
+++ test/anderson_darling.jl
@@ -7,10 +7,12 @@ srand(1984948)
 x = rand(Normal(), n)
 t = OneSampleADTest(x, Normal())
 @test_approx_eq_eps t.A² 0.2013 0.1^4
+@test_approx_eq_eps pvalue(t) 0.8811 0.1^4
 
 x = rand(DoubleExponential(), n)
 t = OneSampleADTest(x, Normal())
 @test_approx_eq_eps t.A² 10.7439 0.1^4
+@test_approx_eq_eps pvalue(t) 0.0 0.1^4
 
 x = rand(Cauchy(), n)
 t = OneSampleADTest(x, Normal())
