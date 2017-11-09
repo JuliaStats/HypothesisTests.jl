43c30df6c812d9fb52ff611c7f40508496ddb924
Art <wildart@gmail.com>
2017-11-09 18:32:49 -0500

    review fixes

--- test/anderson_darling.jl
+++ test/anderson_darling.jl
@@ -7,34 +7,34 @@ srand(1984948)
 
 x = rand(Normal(), n)
 t = OneSampleADTest(x, Normal())
-@test isapprox(t.A², 0.2013, atol=0.1^4)
-@test isapprox(pvalue(t), 0.99, atol=0.1^4)
+@test t.A² ≈ 0.2013 atol=0.1^4
+@test pvalue(t) ≈ 0.99 atol=0.1^4
 @test default_tail(t) == :right
 
 x = rand(DoubleExponential(), n)
 t = OneSampleADTest(x, Normal())
-@test isapprox(t.A², 10.7439, atol=0.1^4)
-@test isapprox(pvalue(t), 0.0, atol=0.1^4)
+@test t.A² ≈ 10.7439 atol=0.1^4
+@test pvalue(t) ≈ 0.0 atol=0.1^4
 
 x = rand(Cauchy(), n)
 t = OneSampleADTest(x, Normal())
-@test isapprox(pvalue(t), 0.0, atol=0.1^4)
+@test pvalue(t) ≈ 0.0 atol=0.1^4
 
 x = rand(LogNormal(), n)
 t = OneSampleADTest(x, Normal())
-@test isapprox(pvalue(t), 0.0, atol=0.1^4)
+@test pvalue(t) ≈ 0.0 atol=0.1^4
 
 x = rand(Uniform(), n)
 t = OneSampleADTest(x, Uniform())
-@test isapprox(pvalue(t), 0.145, atol=0.1^4)
+@test pvalue(t) ≈ 0.145 atol=0.1^4
 
 x = rand(Uniform(0, 1.8), n)
 t = OneSampleADTest(x, Uniform())
-@test isapprox(pvalue(t), 0.5112, atol=0.1^4)
+@test pvalue(t) ≈ 0.5112 atol=0.1^4
 
 x = rand(Exponential(), n)
 t = OneSampleADTest(x, Exponential())
-@test isapprox(pvalue(t), 0.9427, atol=0.1^4)
+@test pvalue(t) ≈ 0.9427 atol=0.1^4
 
 # k-sample test
 srand(948574875)
@@ -46,21 +46,21 @@ samples = Any[
 ]
 
 t = KSampleADTest(samples...)
-@test isapprox(t.A²k, 8.3926, atol=0.1^4)
-@test isapprox(t.σ, 1.2038, atol=0.1^4)
-@test isapprox(pvalue(t), 0.0022, atol=0.1^4)
+@test t.A²k ≈ 8.3926 atol=0.1^4
+@test t.σ ≈ 1.2038 atol=0.1^4
+@test pvalue(t) ≈ 0.0022 atol=0.1^4
 @test default_tail(t) == :right
 
 ts = KSampleADTest(samples..., nsim = 20000);
-@test isapprox(pvalue(ts), 0.00155, atol=0.1^3)
+@test pvalue(ts) ≈ 0.00155 atol=0.1^3
 
 t = KSampleADTest(samples..., modified = false)
-@test isapprox(t.A²k, 8.3559, atol=0.1^4)
-@test isapprox(t.σ, 1.2038, atol=0.1^4)
-@test isapprox(pvalue(t), 0.0023, atol=0.1^4)
+@test t.A²k ≈ 8.3559 atol=0.1^4
+@test t.σ ≈ 1.2038 atol=0.1^4
+@test pvalue(t) ≈ 0.0023 atol=0.1^4
 
 ts = KSampleADTest(samples..., modified = false, nsim = 20000);
-@test isapprox(pvalue(ts), 0.00150, atol=0.1^3)
+@test pvalue(ts) ≈ 0.00150 atol=0.1^3
 
 # more tests
 srand(31412455)
@@ -72,4 +72,4 @@ samples = Any[rand(Normal(), 50), rand(Normal(), 30), rand(Normal(), 20)]
 t = KSampleADTest(samples...)
 @test pvalue(t) > 0.05
 
-@test isapprox(pvalue(OneSampleADTest(vcat(rand(Normal(),500), rand(Beta(2,2),500)), Beta(2,2))), 0.0, atol=0.1^4)
+@test pvalue(OneSampleADTest(vcat(rand(Normal(),500), rand(Beta(2,2),500)), Beta(2,2))) ≈ 0.0 atol=0.1^4
