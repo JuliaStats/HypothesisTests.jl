ce7d35a044a2705e8519d69feb442e22da075b7b
LawrenceMz <36506780+LawrenceMz@users.noreply.github.com>
2019-05-19 15:35:35 -0500

    Update documentation

--- src/fisher.jl
+++ src/fisher.jl
@@ -26,7 +26,7 @@ export FisherExactTest
 
 """
     FisherExactTest(a::Integer, b::Integer, c::Integer, d::Integer)
-    FisherExactTest(x)
+    FisherExactTest(x[, y])
 
 Perform Fisher's exact test of the null hypothesis that the success probabilities ``a/c``
 and ``b/d`` are equal, that is the odds ratio ``(a/c) / (b/d)`` is one, against the
