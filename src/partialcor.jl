cc22cd841e36b85fbe0e12c2de13c2911a098513
Alex Arslan <ararslan@comcast.net>
2019-10-10 10:35:38 -0700

    Apply suggestions from code review

--- src/partialcor.jl
+++ src/partialcor.jl
@@ -10,25 +10,25 @@ correlation of vectors `x` and `y` given the matrix `Z` is zero.
 
 Implements `pvalue` and `confint`. See also [`partialcor`](@ref).
 """
-struct PartialCorTest <: HypothesisTest
-    r::Real
+struct PartialCorTest{T<:Real} <: HypothesisTest
+    r::T
     n::Int
     k::Int
-    t::Real
+    t::T
 
     # Error checking is done in `partialcor`
     function PartialCorTest(x::AbstractVector, y::AbstractVector, Z::AbstractMatrix)
         r = partialcor(x, y, Z)
         n, k = size(Z)
         t = r * sqrt((n - 2 - k) / (1 - r^2))
-        return new(r, n, k, t)
+        return new{typeof(r)}(r, n, k, t)
     end
 
     function PartialCorTest(x::AbstractVector, y::AbstractVector, z::AbstractVector)
         r = partialcor(x, y, z)
         n = length(z)
         t = r * sqrt((n - 3) / (1 - r^2))
-        return new(r, n, 1, t)
+        return new{typeof(r)}(r, n, 1, t)
     end
 end
 
@@ -38,15 +38,16 @@ population_param_of_interest(p::PartialCorTest) = ("Partial correlation", zero(p
 StatsBase.nobs(p::PartialCorTest) = p.n
 StatsBase.dof(p::PartialCorTest) = p.n - 2 - p.k
 
-function StatsBase.confint(test::PartialCorTest, alpha::Float64=0.05)
-    dof(test) > 1 || return (-one(test.r), one(test.r)) # Otherwise we can get NaNs
-    q = quantile(Normal(), 1 - alpha / 2)
-    fisher = (log1p(test.r) - log1p(-test.r)) / 2
+function StatsBase.confint(test::PartialCorTest{T}, alpha::Float64=0.05) where T
+    df = dof(test)
+    df > 1 || return (-one(T), one(T))  # Otherwise we can get NaNs
+    q = quantile(TDist(df), 1 - alpha / 2)
+    fisher = atanh(test.r)
     bound = q / sqrt(test.n - 3 - test.k)
     lo = 2 * (fisher - bound)
     hi = 2 * (fisher + bound)
-    elo = Statistics.clampcor(expm1(lo) / (exp(lo) + 1))
-    ehi = Statistics.clampcor(expm1(hi) / (exp(hi) + 1))
+    elo = Statistics.clampcor(tanh(fisher - bound))
+    ehi = Statistics.clampcor(tanh(fisher + bound))
     return (elo, ehi)
 end
 
