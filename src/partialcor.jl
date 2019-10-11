5cbbc2d94f6bd4a6d2664780d3fdbaf7177d22cd
Alex Arslan <ararslan@comcast.net>
2019-10-11 10:22:09 -0700

    Apply suggestions from code review

--- src/partialcor.jl
+++ src/partialcor.jl
@@ -1,5 +1,7 @@
 # Partial correlation
 
+using Statistics: clampcor
+
 export PartialCorTest
 
 """
@@ -8,7 +10,18 @@ export PartialCorTest
 Perform a t-test for the hypothesis that ``\\text{Cor}(x,y|Z=z) = 0``, i.e. the partial
 correlation of vectors `x` and `y` given the matrix `Z` is zero.
 
-Implements `pvalue` and `confint`. See also [`partialcor`](@ref).
+Implements `pvalue` for the t-test.
+Implements `confint` using an approximate confidence interval based on Fisher's
+``z``-transform.
+
+See also `partialcor` from StatsBase.
+
+# External resources
+
+* [Partial correlation on Wikipedia](https://en.wikipedia.org/wiki/Partial_correlation#As_conditional_independence_test)
+  (for the construction of the confidence interval)
+* [Section testing using Student's t-distribution from Pearson correlation coefficient on Wikipedia](https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#Testing_using_Student's_t-distribution)
+* [K.J. Levy and S.C. Narula (1978): Testing Hypotheses concerning Partial Correlations: Some Methods and Discussion. International Statistical Review 46(2).](https://www.jstor.org/stable/1402814)
 """
 struct PartialCorTest{T<:Real} <: HypothesisTest
     r::T
@@ -39,15 +52,12 @@ StatsBase.nobs(p::PartialCorTest) = p.n
 StatsBase.dof(p::PartialCorTest) = p.n - 2 - p.k
 
 function StatsBase.confint(test::PartialCorTest{T}, alpha::Float64=0.05) where T
-    df = dof(test)
-    df > 1 || return (-one(T), one(T))  # Otherwise we can get NaNs
-    q = quantile(TDist(df), 1 - alpha / 2)
+    dof(test) > 1 || return (-one(T), one(T))  # Otherwise we can get NaNs
+    q = quantile(Normal(), 1 - alpha / 2)
     fisher = atanh(test.r)
     bound = q / sqrt(test.n - 3 - test.k)
-    lo = 2 * (fisher - bound)
-    hi = 2 * (fisher + bound)
-    elo = Statistics.clampcor(tanh(fisher - bound))
-    ehi = Statistics.clampcor(tanh(fisher + bound))
+    elo = clampcor(tanh(fisher - bound))
+    ehi = clampcor(tanh(fisher + bound))
     return (elo, ehi)
 end
 
