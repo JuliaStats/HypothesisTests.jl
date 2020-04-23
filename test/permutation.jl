ec80ae6c7fa69fa0505b303ad238356cfa6b58b2
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-04-23 14:44:33 -0400

    fix direction of comparison for two-tailed permutation test
    previously counted number of permuted test statistics LESS extreme than the
    observed statistic.  this makes the both-tail behavior consisten with right/left
    tail behavior by computing the fraction of permuted statistics that are as
    or more extreme.  the tests have been updated to reflect that this makes the
    two-tailed test more conservative (because ties count towards h0 under this
    definition).

--- test/permutation.jl
+++ test/permutation.jl
@@ -1,14 +1,14 @@
 using HypothesisTests, Test
 
 @testset "Permutation" begin
-@test isapprox(pvalue(ExactPermutationTest([1,2,3],[4,5,6],mean), tail=:both), 1.0)
-@test isapprox(pvalue(ExactPermutationTest([4,5,6],[1,2,3],mean), tail=:both), 1.0)
+@test isapprox(pvalue(ExactPermutationTest([1,2,3],[4,5,6],mean), tail=:both), 0.1)
+@test isapprox(pvalue(ExactPermutationTest([4,5,6],[1,2,3],mean), tail=:both), 0.1)
 @test isapprox(pvalue(ExactPermutationTest([1,2,3],[4,5,6],mean), tail=:left), 0.05)
 @test isapprox(pvalue(ExactPermutationTest([4,5,6],[1,2,3],mean), tail=:right), 0.05)
 
 Random.seed!(12345)
-@test isapprox(pvalue(ApproximatePermutationTest([1,2,3],[4,5,6],mean,100), tail=:both), 1.0)
-@test isapprox(pvalue(ApproximatePermutationTest([4,5,6],[1,2,3],mean,100), tail=:both), 1.0)
+@test isapprox(pvalue(ApproximatePermutationTest([1,2,3],[4,5,6],mean,100), tail=:both), 0.09)
+@test isapprox(pvalue(ApproximatePermutationTest([4,5,6],[1,2,3],mean,100), tail=:both), 0.04)
 @test isapprox(pvalue(ApproximatePermutationTest([1,2,3],[4,5,6],mean,100), tail=:left), 0.08)
 @test isapprox(pvalue(ApproximatePermutationTest([4,5,6],[1,2,3],mean,100), tail=:right), 0.07)
 end
