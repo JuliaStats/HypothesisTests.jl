725eb26e4efd6a955155f7e3e26062ddf678af8e
Paul Söderlind <PaulSoderlind@users.noreply.github.com>
2020-06-05 15:09:53 +0200

    better description, added a reference

--- src/white.jl
+++ src/white.jl
@@ -35,11 +35,13 @@ end
 
 Compute White's (or Breusch-Pagan's) test for heteroskedasticity.
 
-`X` is the matrix of regressors from the original model and `e` the vector of residuals.
-`TestType` is either `:Linear` for the Breusch-Pagan test, `:LinearAndSquares` for
+`X` is a matrix of regressors and `e` is the vector of residuals from the original model.
+`TestType` is either `:Linear` for the Breusch-Pagan/Koenker test, `:LinearAndSquares` for
 White's test with linear and squared terms only (no cross-products), or `:White` (the default)
 for the full White's test (linear, squared and cross-product terms). `X` should include a
-constant. The test statistic is T*R2 where R2 is from the regression of `e^2` on the terms
+constant. In some applications, `X` is a subset of the regressors in the original model,
+or just the fitted values. This saves degrees of freedom and may give a more powerful test.
+The test statistic is T*R2 where R2 is from the regression of `e^2` on the terms
 mentioned above. Under the null hypothesis it is distributed as `Chisquare(dof)`
 where `dof` is the number of independent terms (not counting the constant), so the null
 is rejected when the test statistic is large enough.
@@ -49,6 +51,7 @@ Implements: [`pvalue`](@ref)
 # References
   * H. White, (1980): A heteroskedasticity-consistent covariance matrix estimator and a direct test for heteroskedasticity, Econometrica, 48, 817-838.
   * T.S. Breusch & A.R. Pagan (1979), A simple test for heteroscedasticity and random coefficient variation, Econometrica, 47, 1287-1294
+  * R. Koenker (1981), A note on studentizing a test for heteroscedasticity, Journal of Econometrics, 17, 107-112
 
 # External links
   * [White's test on Wikipedia](https://en.wikipedia.org/wiki/White_test)
