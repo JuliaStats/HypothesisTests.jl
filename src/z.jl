1121adc75b90da7eb8533df2cad3f9e59186a45e
KronosTheLate <61620837+KronosTheLate@users.noreply.github.com>
2022-02-06 17:12:54 +0100

    Update z.jl

--- src/z.jl
+++ src/z.jl
@@ -171,7 +171,8 @@ Perform a two-sample z-test of the null hypothesis that `x` and `y` come from di
 with different variances and mean difference μ0 (defaults to 0) against the alternative hypothesis
 that the distributions have different variances and a mean difference different from μ0.
 
-For testing if two datasets have equal variance, see [`VarianceFTest`](@ref).
+See also:  [`VarianceFTest`](@ref) to test whether two datasets have equal variance.
+    
 Implements: [`pvalue`](@ref), [`confint`](@ref)
 """
 function UnequalVarianceZTest(x::AbstractVector{T}, y::AbstractVector{S}, μ0::Real=0) where {T<:Real,S<:Real}
