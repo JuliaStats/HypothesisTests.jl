d36355aa5a4e221296c1a2381436b012a3ac6bc2
Marek Kaluba <marek.kaluba@kit.edu>
2023-08-13 16:00:27 +0200

    explain argument `N` in ShapiroWilkCoefs
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/shapiro_wilk.jl
+++ src/shapiro_wilk.jl
@@ -26,7 +26,7 @@ end
     HypothesisTests.ShapiroWilkCoefs(N::Integer)
 
 Construct a vector of de-correlated expected order statistics for Shapiro-Wilk
-test.
+test for a sample of size `N`.
 
 If multiple tests on samples of size `N` are performed, it is beneficial to
 construct and pass a single vector of coefficients to `ShapiroWilkTest`(@ref).
